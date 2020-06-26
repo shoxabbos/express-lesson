ce1d25acc0f5f54d08c3ee034d2632e1d1358519
Shoxabbos Olimjonov <19941001@inbox.ru>
2020-06-26 14:17:59 +0500

    Prettier config

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+let posts = [
+    {
+        title: "My first blog news",
+        content: "This is just content",
+    },
+];
+
+app.set("view engine", "twig");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.render("index", { posts: posts }));
+app.get("/create", (req, res) => res.render("create"));
+app.post("/create", (req, res) => {
+    posts.push({
+        title: req.body.title,
+        content: req.body.content,
+    });
+
+    res.redirect("/");
+});
+
 app.listen(3000, () => console.log("Example app listining an port 3000"));
