845c8f9db93f4807f528e874b6f554440bdf0e62
Shoxabbos Olimjonov <19941001@inbox.ru>
2020-06-26 15:40:35 +0500

    select insert to mongo

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const Post = require("./models/post");
+
 let posts = [
     {
         title: "My first blog news",
@@ -12,13 +14,17 @@ let posts = [
 app.set("view engine", "twig");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.render("index", { posts: posts }));
+app.get("/", (req, res) => {
+    Post.find({}).then((posts) => {
+        res.render("index", { posts: posts });
+    });
+});
+
 app.get("/create", (req, res) => res.render("create"));
 app.post("/create", (req, res) => {
-    posts.push({
-        title: req.body.title,
-        content: req.body.content,
-    });
+    const { title, content } = req.body;
+
+    Post.create({ title, content }).then((post) => console.log(post));
 
     res.redirect("/");
 });
